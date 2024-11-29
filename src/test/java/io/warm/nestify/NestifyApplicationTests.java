2e4a9dad964d04f71d56e87a61ccf2f9de303998
isun-dev <florachoi1124@naver.com>
2024-11-29 16:18:42 +0900

    Initial commit

---
+++ src/test/java/io/warm/nestify/NestifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.warm.nestify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
