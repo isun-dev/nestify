0283f5278014d51af9e32e362adda01c2dc39c0a
isun-dev <florachoi1124@naver.com>
2024-11-29 16:51:32 +0900

    [NESTIFY-1] 환경설정 추가

---
+++ src/main/java/io/warm/nestify/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package io.warm.nestify.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Nestify Swagger")
+                .description("Nestify - 주거 공간 마련")
+                .version("1.0.0");
+    }
+}
+
